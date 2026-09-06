59d4103eaf670fe2997608004f1daddc45486834
sofian Emad <117307449+sofain1@users.noreply.github.com>
2026-09-06 06:22:15 +0300

    Add variables and data types example

---
+++ fundamentals/variables.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int age = 21;
+    double height = 175.5;
+    char grade = 'A';
+    bool isStudent = true;
+
+    cout << "Age: " << age << endl;
+    cout << "Height: " << height << " cm" << endl;
+    cout << "Grade: " << grade << endl;
+    cout << "Student: " << isStudent << endl;
+
+    return 0;
+}
